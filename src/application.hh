3e9df03ec7d6885f9dd2db01c33d2d41a45bde08
ades <anders.lindholsson@gmail.com>
2022-03-31 18:31:14 +0200

    Refactor into app and entry point

---
+++ src/application.hh
@@ -0,0 +1,16 @@
+#include <chrono>
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+
+struct Application {
+private:
+    GLFWwindow *m_window;
+    int m_width;
+    int m_height;
+    std::chrono::time_point<std::chrono::system_clock> m_start_time;
+public:
+    Application(const int width, const int height);
+    ~Application();
+};
