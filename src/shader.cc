c013cd0699386f472e67b29d978fee7113b7e64b
ades <anders.lindholsson@gmail.com>
2022-04-01 19:29:15 +0200

    Refactor

--- src/shader.cc
+++ src/shader.cc
@@ -5,46 +5,13 @@
 
 #include <spdlog/spdlog.h>
 
-#include <GLFW/glfw3.h>
-#include <glad/glad.h>
-
-const std::string read_file(const std::string &fname) {
-  std::ifstream ifs(fname, std::ios::in);
-  if (!ifs.is_open()) {
-    spdlog::error("Failed to load file {}", fname);
-    return "";
-  }
-
-  return std::string((std::istreambuf_iterator<char>(ifs)),
-                     (std::istreambuf_iterator<char>()));
-}
-
-Shader::Shader(const std::string &vertex, const std::string &fragment) {
+Shader::Shader(const std::string &vertex, const std::string &fragment)
+    : m_vertex_src(vertex), m_fragment_src(fragment) {
   int success;
   char info_log[512];
 
-  const auto vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  const std::string vertex_shader_src = read_file(vertex);
-  const char *vertex_shader_source_C = vertex_shader_src.c_str();
-  glShaderSource(vertex_shader, 1, &vertex_shader_source_C, nullptr);
-  glCompileShader(vertex_shader);
-
-  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(vertex_shader, 512, nullptr, info_log);
-    spdlog::error("Failed to compile vertex shader", info_log);
-  }
-
-  const auto fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  const std::string fragment_shader_src = read_file(fragment);
-  const char *fragment_shader_src_C = fragment_shader_src.c_str();
-  glShaderSource(fragment_shader, 1, &fragment_shader_src_C, nullptr);
-  glCompileShader(fragment_shader);
-  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(fragment_shader, 512, nullptr, info_log);
-    spdlog::error("Failed to compile fragment shader {}", info_log);
-  }
+  const auto vertex_shader = compile_shader(vertex, GL_VERTEX_SHADER);
+  const auto fragment_shader = compile_shader(fragment, GL_FRAGMENT_SHADER);
 
   m_id = glCreateProgram();
   glAttachShader(m_id, vertex_shader);
@@ -60,12 +27,38 @@ Shader::Shader(const std::string &vertex, const std::string &fragment) {
   glDeleteShader(fragment_shader);
 }
 
-Shader::~Shader() { glDeleteProgram(m_id); }
-
-void Shader::use() { glUseProgram(m_id); }
-
 void Shader::set_vec3(const std::string &name, const float x, const float y,
                       const float z) const {
   const auto location = glGetUniformLocation(m_id, name.c_str());
   glUniform3f(location, x, y, z);
+}
+
+unsigned int Shader::compile_shader(const std::string &path,
+                                    GLenum type) const {
+  int success;
+  char info_log[512];
+
+  const auto shader = glCreateShader(type);
+  const std::string src = read_file(path);
+  const char *src_c = src.c_str();
+  glShaderSource(shader, 1, &src_c, nullptr);
+  glCompileShader(shader);
+
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 512, nullptr, info_log);
+    spdlog::error("Failed to compile shader {}: {}", type, info_log);
+  }
+  return shader;
+}
+
+const std::string Shader::read_file(const std::string &fname) const {
+  std::ifstream ifs(fname, std::ios::in);
+  if (!ifs.is_open()) {
+    spdlog::error("Failed to load file {}", fname);
+    return "";
+  }
+
+  return std::string((std::istreambuf_iterator<char>(ifs)),
+                     (std::istreambuf_iterator<char>()));
 }
--- src/shader.hh
+++ src/shader.hh
@@ -2,16 +2,25 @@
 
 #include <string>
 
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
 class Shader {
 private:
   unsigned int m_id;
+  const std::string m_vertex_src, m_fragment_src;
 
 public:
   Shader(const std::string &vertex, const std::string &fragment);
-  ~Shader();
-
+  ~Shader() { glDeleteProgram(m_id); }
   unsigned int get_id() const { return m_id; }
-  void use();
+
+  void use() const { glUseProgram(m_id); }
+
   void set_vec3(const std::string &name, const float x, const float y,
                 const float z) const;
+
+private:
+  unsigned int compile_shader(const std::string &path, GLenum type) const;
+  const std::string read_file(const std::string &path) const;
 };
