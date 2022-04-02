50c260f81c91aa63378582a96427b1c94c187acc
ades <anders.lindholsson@gmail.com>
2022-04-02 14:52:40 +0200

    Cache uniforms

--- src/shader.cc
+++ src/shader.cc
@@ -6,12 +6,12 @@
 #include <spdlog/spdlog.h>
 
 Shader::Shader(const std::string &vertex, const std::string &fragment)
-    : m_vertex_src(vertex), m_fragment_src(fragment) {
+    : m_id(0), m_vertex_src(vertex), m_fragment_src(fragment) {
   int success;
   char info_log[512];
 
-  const auto vertex_shader = compile_shader(vertex, GL_VERTEX_SHADER);
-  const auto fragment_shader = compile_shader(fragment, GL_FRAGMENT_SHADER);
+  const auto vertex_shader = compile_shader(GL_VERTEX_SHADER, vertex);
+  const auto fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fragment);
 
   m_id = glCreateProgram();
   glAttachShader(m_id, vertex_shader);
@@ -21,40 +21,46 @@ Shader::Shader(const std::string &vertex, const std::string &fragment)
   if (!success) {
     glGetProgramInfoLog(m_id, 512, nullptr, info_log);
     spdlog::error("Failed to link shader program\n{}", info_log);
+  } else {
+    spdlog::debug("Created shader program from {} and {}", vertex, fragment);
   }
 
   glDeleteShader(vertex_shader);
   glDeleteShader(fragment_shader);
 }
 
-void Shader::set_bool(const std::string &name, const bool val) const {
-  const auto location = glGetUniformLocation(m_id, name.c_str());
-  glUniform1i(location, static_cast<int>(val));
+void Shader::set_bool(const std::string &name, const bool val) {
+  glUniform1i(get_uniform_location(name), static_cast<int>(val));
 }
 
-void Shader::set_int(const std::string &name, const int val) const {
-  const auto location = glGetUniformLocation(m_id, name.c_str());
-  glUniform1i(location, val);
+void Shader::set_int(const std::string &name, const int val) {
+  glUniform1i(get_uniform_location(name), val);
 }
 
-void Shader::set_float(const std::string &name, const float val) const {
-  const auto location = glGetUniformLocation(m_id, name.c_str());
-  glUniform1f(location, val);
+void Shader::set_float(const std::string &name, const float val) {
+  glUniform1f(get_uniform_location(name), val);
 }
 
-void Shader::set_vec3(const std::string &name, const glm::vec3 &val) const {
-  const auto location = glGetUniformLocation(m_id, name.c_str());
-  glUniform3fv(location, 1, &val[0]);
+void Shader::set_vec3(const std::string &name, const glm::vec3 &val) {
+  glUniform3fv(get_uniform_location(name), 1, &val[0]);
 }
 
-void Shader::set_vec3(const std::string &name, const float x, const float y,
-                      const float z) const {
-  const auto location = glGetUniformLocation(m_id, name.c_str());
-  glUniform3f(location, x, y, z);
+void Shader::set_vec3(const std::string &name, const float v0, const float v1,
+                      const float v2) {
+  glUniform3f(get_uniform_location(name), v0, v1, v2);
 }
 
-unsigned int Shader::compile_shader(const std::string &path,
-                                    GLenum type) const {
+int Shader::get_uniform_location(const std::string &name) {
+  if (m_cache.find(name) != m_cache.end()) {
+    return m_cache[name];
+  }
+  auto location = glGetUniformLocation(m_id, name.c_str());
+  m_cache[name] = location;
+  return location;
+}
+
+unsigned int Shader::compile_shader(unsigned int type,
+                                    const std::string &path) const {
   int success;
   char info_log[512];
 
@@ -67,7 +73,8 @@ unsigned int Shader::compile_shader(const std::string &path,
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   if (!success) {
     glGetShaderInfoLog(shader, 512, nullptr, info_log);
-    spdlog::error("Failed to compile shader {}: {}", type, info_log);
+    spdlog::error("Failed to compile shader {}: {}",
+                  type == GL_VERTEX_SHADER ? "vertex" : "fragment", info_log);
   }
   return shader;
 }
--- src/shader.hh
+++ src/shader.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
@@ -10,6 +11,7 @@
 class Shader {
 private:
   unsigned int m_id;
+  std::unordered_map<std::string, int> m_cache;
   const std::string m_vertex_src, m_fragment_src;
 
 public:
@@ -19,14 +21,15 @@ public:
 
   void use() const { glUseProgram(m_id); }
 
-  void set_bool(const std::string &name, const bool val) const;
-  void set_int(const std::string &name, const int val) const;
-  void set_float(const std::string &name, const float val) const;
-  void set_vec3(const std::string &name, const glm::vec3 &val) const;
-  void set_vec3(const std::string &name, const float x, const float y,
-                const float z) const;
+  void set_bool(const std::string &name, const bool val);
+  void set_int(const std::string &name, const int val);
+  void set_float(const std::string &name, const float val);
+  void set_vec3(const std::string &name, const glm::vec3 &val);
+  void set_vec3(const std::string &name, const float v0, const float v1,
+                const float v2);
 
 private:
-  unsigned int compile_shader(const std::string &path, GLenum type) const;
+  int get_uniform_location(const std::string &name);
+  unsigned int compile_shader(unsigned int type, const std::string &path) const;
   const std::string read_file(const std::string &path) const;
 };
