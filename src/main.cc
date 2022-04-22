d5ed40aab8105b0dcd7d719f961451e45488a0cb
anderslindho <anders.lindh.olsson@gmail.com>
2022-04-22 11:01:46 +0200

    Appease clang-format

--- src/main.cc
+++ src/main.cc
@@ -6,7 +6,7 @@
 const int k_width = 800;
 const int k_height = 640;
 
-int main(int /* argc */, char **/* argv */) {
+int main(int /* argc */, char ** /* argv */) {
   auto app = new Application(k_width, k_height);
   app->run();
   delete app;
