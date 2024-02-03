94efa9689e717817f41140f34690294eab2f9715
anderslindho <anders.lindh.olsson@gmail.com>
2024-02-03 19:59:23 +0100

    Refactor

--- src/camera.cc
+++ src/camera.cc
@@ -9,26 +9,25 @@ glm::mat4 Camera::get_view_matrix() const {
 }
 
 void Camera::move(Direction direction, float delta_time) {
+  auto magnitude = m_speed * delta_time;
   switch (direction) {
   case Direction::k_forward:
-    m_position += m_direction * m_speed * delta_time;
+    m_position += m_direction * magnitude;
     break;
   case Direction::k_backward:
-    m_position -= m_direction * m_speed * delta_time;
+    m_position -= m_direction * magnitude;
     break;
   case Direction::k_left:
-    m_position -=
-        glm::normalize(glm::cross(m_direction, m_up)) * m_speed * delta_time;
+    m_position -= glm::normalize(glm::cross(m_direction, m_up)) * magnitude;
     break;
   case Direction::k_right:
-    m_position +=
-        glm::normalize(glm::cross(m_direction, m_up)) * m_speed * delta_time;
+    m_position += glm::normalize(glm::cross(m_direction, m_up)) * magnitude;
     break;
   case Direction::k_up:
-    m_position += m_up * m_speed * delta_time;
+    m_position += m_up * magnitude;
     break;
   case Direction::k_down:
-    m_position -= m_up * m_speed * delta_time;
+    m_position -= m_up * magnitude;
     break;
   }
 }
